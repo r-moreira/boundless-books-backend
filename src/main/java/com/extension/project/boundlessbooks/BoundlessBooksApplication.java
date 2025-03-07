916a4e65942c489eead39b1e02776c48d7eb363c
Rodrigo Avila <r.moreira4297@gmail.com>
2025-03-07 18:39:35 -0300

    fix docker build

--- src/main/java/com/extension/project/boundlessbooks/BoundlessBooksApplication.java
+++ src/main/java/com/extension/project/boundlessbooks/BoundlessBooksApplication.java
@@ -18,27 +18,27 @@ public class BoundlessBooksApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        EpubReader epubReader = new EpubReader();
-
-        try (FileInputStream fis = new FileInputStream("/Users/rodrigo/Dev/infnet/extension/boundless-books/src/main/resources/epub/zodiacs-toy-john-jones-obooko.epub")) {
-
-            Book book = epubReader.readEpub(fis);
-
-            System.out.println("Title: " + book.getTitle());
-            System.out.println("Dates: " + book.getMetadata().getDates());
-            System.out.println("Publishers: " + book.getMetadata().getPublishers());
-            System.out.println("Author: " + book.getMetadata().getAuthors());
-            System.out.println("Language: " + book.getMetadata().getLanguage());
-            System.out.println("Description: " + book.getMetadata().getDescriptions());
-
-            // Save cover image file to a file
-            byte[] coverImageData = book.getCoverImage().getData();
-            try (FileOutputStream fos = new FileOutputStream("/Users/rodrigo/Dev/infnet/extension/boundless-books/src/main/resources/epub/cover.jpg")) {
-                fos.write(coverImageData);
-            }
-        } catch (net.lingala.zip4j.exception.ZipException e) {
-            System.err.println("Error reading EPUB file: " + e.getMessage());
-            e.printStackTrace();
-        }
+//        EpubReader epubReader = new EpubReader();
+//
+//        try (FileInputStream fis = new FileInputStream("/Users/rodrigo/Dev/infnet/extension/boundless-books/src/main/resources/epub/zodiacs-toy-john-jones-obooko.epub")) {
+//
+//            Book book = epubReader.readEpub(fis);
+//
+//            System.out.println("Title: " + book.getTitle());
+//            System.out.println("Dates: " + book.getMetadata().getDates());
+//            System.out.println("Publishers: " + book.getMetadata().getPublishers());
+//            System.out.println("Author: " + book.getMetadata().getAuthors());
+//            System.out.println("Language: " + book.getMetadata().getLanguage());
+//            System.out.println("Description: " + book.getMetadata().getDescriptions());
+//
+//            // Save cover image file to a file
+//            byte[] coverImageData = book.getCoverImage().getData();
+//            try (FileOutputStream fos = new FileOutputStream("/Users/rodrigo/Dev/infnet/extension/boundless-books/src/main/resources/epub/cover.jpg")) {
+//                fos.write(coverImageData);
+//            }
+//        } catch (net.lingala.zip4j.exception.ZipException e) {
+//            System.err.println("Error reading EPUB file: " + e.getMessage());
+//            e.printStackTrace();
+//        }
     }
 }
